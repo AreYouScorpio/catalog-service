ec8660ecc53a17cd217f12ba46b60faff642bad5
akostom <akostom@gulfgasandpower.nl>
2024-12-31 01:36:21 +0100

    init cataloge

---
+++ src/main/java/com/webshop/catalog/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.webshop.catalog.controller;
+
+import com.webshop.catalog.model.Category;
+import com.webshop.catalog.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
+    }
+}
---
+++ src/main/java/com/webshop/catalog/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.webshop.catalog.repository;
+
+import com.webshop.catalog.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
+}
